d0a97ca36388603d2310492279e1f5f8411a82f7
hkawaidayodayo <h.kawaidada@gmail.com>
2021-11-04 00:16:02 +0900

    モーダルを実装した

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// $(document).on('mouseover','.death',function(){
+//     location.href = 'http://abehiroshi.la.coocan.jp/';
+// });
+
+// $(function(){
+//     $('button').click(function(){
+//         // ここで画面の配置を作っている
+//       $('body').append('<div id="bg">').append('<div id="modal">');
+
+//       // ここでYOUTUBEを表示
+//       $('#modal').append
+//       ('<iframe width="1200" height="1000" src="https://www.youtube.com/embed/UyDLd_4sSWk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+//         {
+//       }
+
+    //   $('#bg').click(function(){
+    //   // バックグラウンドorモーダルを押した時に消したい
+    //   $('#bg,#modal').remove();
+    //   });
+
+  
+
